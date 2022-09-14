e4c85c31cdde311e6a3f7b762a962852f7a4829b
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-14 02:07:22 +0200

    no message

---
+++ Web App using ASP.Net and CSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_App_using_ASP.Net_and_CSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
